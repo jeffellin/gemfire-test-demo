88f4a86d7fe2024000f284e92ac30e124eb24562
Oleg Iavorskyi <oleg.iavorskyi@gmail.com>
2019-04-09 18:24:51 -0400

    Initial demo with failing GemFire test

---
+++ src/main/java/com/example/gftestdemo/GemfireConfig.java
@@ -0,0 +1,28 @@
+package com.example.gftestdemo;
+
+import org.apache.geode.cache.GemFireCache;
+import org.apache.geode.cache.Region;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.gemfire.GemfireTemplate;
+import org.springframework.data.gemfire.client.ClientRegionFactoryBean;
+
+@Configuration
+public class GemfireConfig {
+
+    @Bean
+    public ClientRegionFactoryBean<String, Boolean> restrictionsRegion(GemFireCache cache) {
+        ClientRegionFactoryBean<String, Boolean> region = new ClientRegionFactoryBean<>();
+
+        region.setCache(cache);
+        region.setLookupEnabled(true);
+
+        return region;
+    }
+
+    @Bean
+    public GemfireTemplate template(Region restrictionsRegion) {
+        return new GemfireTemplate(restrictionsRegion);
+    }
+
+}
